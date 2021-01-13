221059cb430f5163590ac3bc68b88150b84134f8
Daria Bondarenko <da.bondarenko@sov-teh.com>
2021-01-13 19:32:57 +0300

    Create some layouts

--- 04-SPA/04-Layouts/components/BaseLayout.vue
+++ 04-SPA/04-Layouts/components/BaseLayout.vue
@@ -1,8 +1,19 @@
-<template></template>
+<template>
+  <div>
+    <the-header />
+    <main>
+      <router-view />
+    </main>
+    <the-footer />
+  </div>
+</template>
 
 <script>
+import TheHeader from './TheHeader';
+import TheFooter from './TheFooter';
 export default {
   name: 'BaseLayout',
+  components: { TheHeader, TheFooter },
 };
 </script>
 
