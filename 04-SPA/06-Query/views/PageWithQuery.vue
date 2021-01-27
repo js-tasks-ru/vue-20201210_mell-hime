007926b0d4a60f94cee9323b866c5b6f0bf61dcd
Daria Bondarenko <da.bondarenko@sov-teh.com>
2021-01-27 11:33:13 +0300

    Page with Query

--- 04-SPA/06-Query/views/PageWithQuery.vue
+++ 04-SPA/06-Query/views/PageWithQuery.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <meetups-view />
+    <meetups-view v-bind.sync="$data" />
   </div>
 </template>
 
@@ -10,6 +10,38 @@ import MeetupsView from '../components/MeetupsView';
 export default {
   name: 'PageWithQuery',
   components: { MeetupsView },
+
+  data() {
+    return {
+      search: this.$route.query.search,
+      date: this.$route.query.date,
+      participation: this.$route.query.participation,
+      view: this.$route.query.view,
+    };
+  },
+
+  computed: {
+    updatedQuery: {
+      get: function () {
+        return this.search;
+      },
+      set: function (query) {
+        this.search = query.search;
+        this.date = query.date;
+        this.participation = query.participation;
+        this.view = query.view;
+      },
+    },
+  },
+
+  watch: {
+    $route(to, from) {
+      this.updatedQuery = to.query;
+    },
+    search: function (oldValue, newValue) {
+      this.$router.replace({ path: '/', query: { search: newValue } });
+    },
+  },
 };
 </script>
 
