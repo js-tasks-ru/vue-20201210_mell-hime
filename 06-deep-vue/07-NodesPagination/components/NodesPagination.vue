38146d3fbe0817fba42ad7afcdf012b21f3ae04f
Daria Bondarenko <da.bondarenko@sov-tech.com>
2021-03-11 18:55:27 +0300

    create Nodes Pagination Component

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,31 @@
 <script>
+const chunk = function (array, size) {
+  if (array === undefined || !array.length) {
+    return [];
+  }
+  const head = array.slice(0, size);
+  const tail = array.slice(size);
+
+  return [head, ...chunk(tail, size)];
+};
+
 export default {
   name: 'NodesPagination',
+
+  props: {
+    page: {
+      type: Number,
+    },
+    perPage: {
+      type: Number,
+    },
+  },
+
+  render: function (createElement) {
+    const data = chunk(this.$slots.default, this.perPage)[this.page - 1];
+
+    return createElement('div', data);
+  },
 };
 </script>
 
