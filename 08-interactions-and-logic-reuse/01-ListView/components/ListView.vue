e54f9976bc703bd52e2bac452f210a16775ac703
Daria Bondarenko <da.bondarenko@sov-tech.com>
2021-04-14 15:12:52 +0300

    List view with scoped slot

--- 08-interactions-and-logic-reuse/01-ListView/components/ListView.vue
+++ 08-interactions-and-logic-reuse/01-ListView/components/ListView.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="meetups-list">
-    <!-- Требуется scoped-slot для каждого элемента списка -->
+    <template v-for="item in items">
+      <slot :item="item">
+        <span>{{ item }}</span>
+      </slot>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ListView',
+  props: {
+    items: Array,
+  },
 };
 </script>
 
