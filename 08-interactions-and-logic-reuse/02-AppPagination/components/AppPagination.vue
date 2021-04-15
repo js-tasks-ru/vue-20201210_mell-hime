133769daa232e2d4e34020ff54db33c6fca628b9
Daria Bondarenko <da.bondarenko@sov-tech.com>
2021-04-15 15:49:53 +0300

    App Pagination with slot

--- 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
+++ 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-    <!-- Решение -->
+    <template v-for="item in paginatedItems">
+      <slot :item="item">
+        <span>{{ item }}</span>
+      </slot>
+    </template>
   </div>
 </template>
 
 <script>
+const chunk = function (array, size) {
+  if (array === undefined || !array.length) {
+    return [];
+  }
+  const head = array.slice(0, size);
+  const tail = array.slice(size);
+
+  return [head, ...chunk(tail, size)];
+};
+
 export default {
   name: 'AppPagination',
+  props: {
+    page: Number,
+    perPage: Number,
+    items: Array,
+  },
+
+  computed: {
+    paginatedItems() {
+      return chunk(this.items, this.perPage)[this.page - 1];
+    },
+  },
 };
 </script>
 
